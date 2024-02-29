ffc8ec7cd669d9baa24875b96a3569ca84f22e6c
kalsaitgaurav@gmail.com <kalsaitgaurav@gmail.com>
2024-02-29 13:23:18 +0530

    Adding Song Sections

--- src/App.js
+++ src/App.js
@@ -2,15 +2,50 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import styles from './App.module.css'
 import Hero from './components/Hero/Hero'
+import Section from './components/Section/Section'
+import { fetchTopAlbums, fetchNewAlbums } from './api/api'
+import {  useState } from 'react' 
 
 
 function App() {
+
+  const [topAlbumSongs, setTopAlbumSongs] = useState([])
+  const [newAlbumSongs, setNewAlbumSongs] = useState([])
+
+  const generateTopAlbumSongs = async () => {
+    try {
+      const topAlbumSongs = await fetchTopAlbums()
+      setTopAlbumSongs(topAlbumSongs)
+    }
+    catch (error) {
+      console.log(error)
+      return null
+    }
+
+  }
+
+    const generateNewAlbumSongs = async () => {
+      try {
+        const newAlbumSongs = await fetchNewAlbums()
+        setNewAlbumSongs(newAlbumSongs);
+       
+      }
+      catch (error) {
+        console.log(error)
+        return null
+      }
+    }
+
+    generateTopAlbumSongs();
+    generateNewAlbumSongs();
   
   return (
     <>
       <Navbar />
       <Hero />
       <div className={styles.sectionWrapper}>
+      <Section type='album' title='Top Albums' data={topAlbumSongs} />
+        <Section type='album' title='New Albums' data={newAlbumSongs} />
       </div>
     </>
   )
