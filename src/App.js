2fc15cb736f80c04512894f8abdcef3e84a2024b
kalsaitgaurav@gmail.com <kalsaitgaurav@gmail.com>
2024-02-29 12:46:54 +0530

    Add NavBar new files and update CSS styles

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar.jsx'
+import styles from './App.module.css'
+
+
+function App() {
+  
+  return (
+    <>
+      <Navbar />
+      <div className={styles.sectionWrapper}>
+      </div>
+    </>
+  )
+}
+
+export default App
