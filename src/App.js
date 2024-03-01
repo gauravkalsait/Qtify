7e0db686a459a4f8a8dadc635b56e28292eb7ba9
kalsaitgaurav@gmail.com <kalsaitgaurav@gmail.com>
2024-03-01 10:34:17 +0530

    Final-Modd

--- src/App.js
+++ src/App.js
@@ -3,14 +3,71 @@ import Navbar from './components/Navbar/Navbar'
 import styles from './App.module.css'
 import Hero from './components/Hero/Hero'
 import Section from './components/Section/Section'
-import { fetchTopAlbums, fetchNewAlbums } from './api/api'
-import {  useState } from 'react' 
+import { fetchTopAlbums, fetchNewAlbums, fetchSongs } from './api/api'
+import {  useState, useEffect } from 'react' 
+import FilterSection from './components/FilterSection/FilterSection'
+
 
 
 function App() {
 
   const [topAlbumSongs, setTopAlbumSongs] = useState([])
   const [newAlbumSongs, setNewAlbumSongs] = useState([])
+  const [filteredDataValues, setFilteredDataValues] = useState([''])
+  const [toggle, setToggle] = useState(false)
+  const [value, setValue] = useState(0);
+
+
+  const generateSongsData = (value) => {
+
+    let songData = newAlbumSongs[0].songs;
+
+    let key;
+    if (value === 0) {
+
+      setFilteredDataValues(songData)
+      return;
+    }
+
+    else if (value === 1) {
+      key = 'rock'
+
+    }
+
+    else if (value === 2) {
+      key = 'pop'
+    }
+
+    else if (value === 3) {
+      key = 'jazz'
+    }
+
+    else if (value === 4) {
+      key = 'blues'
+    }
+
+    const data = songData.filter((item) => {
+      return item.genre.key === key
+
+    })
+    setFilteredDataValues(data)
+
+  }
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+
+    generateSongsData(newValue)
+
+  }
+  const handleToggle = () => {
+    setToggle(!toggle)
+  }
+
+  const filteredData = (val) => {
+    generateSongsData(val)
+    
+  }
 
   const generateTopAlbumSongs = async () => {
     try {
@@ -36,8 +93,33 @@ function App() {
       }
     }
 
-    generateTopAlbumSongs();
-    generateNewAlbumSongs();
+    const generateFilterSongs = async () => {
+
+      try {
+        const newAlbumSongs = await fetchSongs()
+        setFilteredDataValues(newAlbumSongs);
+      }
+  
+      catch (error) {
+        console.log(error)
+        return null 
+  
+      }
+    }
+  
+  useEffect(() => {
+      // eslint-disable-next-line
+    }, [value])
+  
+  useEffect(() => {
+      
+      generateTopAlbumSongs();
+      generateNewAlbumSongs();
+      generateFilterSongs();
+      // setFilteredDataValues(newAlbumSongs);
+  
+    }, [])
+  
   
   return (
     <>
@@ -46,6 +128,8 @@ function App() {
       <div className={styles.sectionWrapper}>
       <Section type='album' title='Top Albums' data={topAlbumSongs} />
         <Section type='album' title='New Albums' data={newAlbumSongs} />
+        <FilterSection data={newAlbumSongs} type='songFilter' title='Songs' filteredData={filteredData} filteredDataValues={filteredDataValues} value={value} handleChange={handleChange} handleToggle={handleToggle}/>
+
       </div>
     </>
   )
